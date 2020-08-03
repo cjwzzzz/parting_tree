235544a6be377e4c8248072b3923f0050db6bb9a
cjwzzzz <39049421+cjwzzzz@users.noreply.github.com>
2020-08-03 10:49:24 +0800

    Update path_Par1.py

--- path_Par1.py
+++ path_Par1.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+import matplotlib.pyplot as plt
 class Net:
     def __init__(self, netId, subnet_id,pin1_X, pin1_Y,pin1_Layer, pin2_X, pin2_Y,pin2_Layer,flag):
         self.netId = netId
@@ -11,14 +12,45 @@ class Net:
         self.pin2_Layer = pin2_Layer              
         self.flag = flag
 
+def plot_branch():
+    global path_List_Over_Plot
+    print(path_List_Over_Plot)
+    np.random.seed(19680801)
+    N=len(path_List_Over_Plot)
+    color0=np.random.random(N)
+    color1=np.random.random(N)
+    color2=np.random.random(N)
+    # print(color0)
+    # print(color1)
+    # print(color2)
+    i=0
+    fig=plt.figure()
+    for x in path_List_Over_Plot:
+        print(x)
+        
+        branch_x=[]
+        branch_y=[]
+        for xx in x[:-3]:
+            branch_x.append(xx[0])
+            branch_y.append(xx[1])
+        
+        #plt.subplot(N,1,i+1)
+        
+        plt.scatter(branch_x,branch_y,c=(color0[i],color1[i],color2[i]),s=2)
+        i+=1
+
+        
+    plt.show()
+
 def yes1(cur):
     global list1,list0,pinlist
-    global pathdic
+    global pathdic,pathLength
     global pin_List_Write,pin_List_Over
     global path_List_Write,path_List_Over
     for x in list0:
         #print(cur)  问题：丢失了转弯节点
-        pin_List_Write.append((x[1],x[2]))
+        # pin_List_Write.append((x[1],x[2]))
+        pathLength+=1
         path_List_Write.append((x[1],x[2]))
         if(x[0]==0):
             first_Flag='0'
@@ -27,8 +59,9 @@ def yes1(cur):
 
         cur=(x[3],x[4])
         while(1):            
-            #print(cur)
+
             path_List_Write.append(cur)
+            pathLength+=1
             if(pathdic[cur][0]>1 ):
                 if(cur in pinlist):
                     last_Flag='0'
@@ -38,43 +71,58 @@ def yes1(cur):
                     last_Flag='1'
                     for i in range(pathdic[cur][0]):                        
                         list1.append((1,cur[0],cur[1],pathdic[cur][2*i+1],pathdic[cur][2*i+2]))            #进入函数  
-                pin_List_Write.append(cur)  
-                pin_List_Write.append(first_Flag+last_Flag)   
-                pin_List_Over.append(pin_List_Write)    
-                pin_List_Write=[]        
-                #pin_List_Write.append('\n')
+                # pin_List_Write.append(cur)  
+                # pin_List_Write.append(first_Flag+last_Flag)   
+                # pin_List_Over.append(pin_List_Write)    
+                # pin_List_Write=[]        
+                
+                path_List_Write.append(first_Flag+last_Flag)
+                path_List_Write.append(curId)
+                path_List_Write.append(pathLength)
+                
                 path_List_Over.append(path_List_Write)
-                #path_List_Write.append('\n')
+                
                 path_List_Write=[]
+                pathLength=0
 
                     
                 break          #when stop while(1)?
             if(pathdic[cur][0]==0):
-                # if(cur in pinlist):
+                
                 last_Flag='0'
-                # else:
-                #     last_Flag='1'
-                pin_List_Write.append(cur) 
-                pin_List_Write.append(first_Flag+last_Flag) 
 
-                pin_List_Over.append(pin_List_Write)    
-                pin_List_Write=[]      
-                #pin_List_Write.append('\n')
+                # pin_List_Write.append(cur) 
+                # pin_List_Write.append(first_Flag+last_Flag) 
+
+                # pin_List_Over.append(pin_List_Write)    
+                # pin_List_Write=[]      
+
+                path_List_Write.append(first_Flag+last_Flag)
+                path_List_Write.append(curId)
+                path_List_Write.append(pathLength)
+                
                 path_List_Over.append(path_List_Write)
-                #path_List_Write.append('\n')
+
                 path_List_Write=[]
+                pathLength=0
                 break
             if(cur  in pinlist and pathdic[cur][0]==1):
                 last_Flag='0'
-                pin_List_Write.append(cur)
-                pin_List_Write.append(first_Flag+last_Flag) 
-                pin_List_Over.append(pin_List_Write)    
-                pin_List_Write=[]     
-                #pin_List_Write.append('\n')
+                # pin_List_Write.append(cur)
+                # pin_List_Write.append(first_Flag+last_Flag) 
+                # pin_List_Over.append(pin_List_Write)    
+                # pin_List_Write=[]     
+
+                path_List_Write.append(first_Flag+last_Flag)
+                path_List_Write.append(curId)
+                path_List_Write.append(pathLength)
+                
                 path_List_Over.append(path_List_Write)
-                #path_List_Write.append('\n')
+
                 path_List_Write=[]
-                pin_List_Write.append(cur)
+                pathLength=0
+                # pin_List_Write.append(cur)
+                pathLength+=1
                 path_List_Write.append(cur)
                 cur=(pathdic[cur][1],pathdic[cur][2])
                 first_Flag='0'              #attention: 没有2个以上节点，但进入新的一行
@@ -83,7 +131,8 @@ def yes1(cur):
     list0=[]
     if(len(list1)!=0):
         for x in list1:
-            pin_List_Write.append((x[1],x[2]))
+            # pin_List_Write.append((x[1],x[2]))
+            pathLength+=1
             path_List_Write.append((x[1],x[2]))
             if(x[0]==0):
                 first_Flag='0'
@@ -91,8 +140,9 @@ def yes1(cur):
                 first_Flag='1'
             cur=(x[3],x[4])
             while(1):
+                pathLength+=1
                 path_List_Write.append(cur)
-                #print(cur)
+
                 if(pathdic[cur][0]>1):
 
                     if(cur in pinlist):
@@ -107,44 +157,60 @@ def yes1(cur):
                             list1.append((1,cur[0],cur[1],pathdic[cur][2*i+1],pathdic[cur][2*i+2]))            #进入函数  
 
 
-                        #list0.append(pathdic[cur][2*i+1])
-                    pin_List_Write.append(cur)
-                    pin_List_Write.append(first_Flag+last_Flag) 
-                    pin_List_Over.append(pin_List_Write)    
-                    pin_List_Write=[]      
-                    #pin_List_Write.append('\n')
+
+                    # pin_List_Write.append(cur)
+                    # pin_List_Write.append(first_Flag+last_Flag) 
+                    # pin_List_Over.append(pin_List_Write)    
+                    # pin_List_Write=[]      
+
+                    path_List_Write.append(first_Flag+last_Flag)
+                    path_List_Write.append(curId)
+                    path_List_Write.append(pathLength)
+                    
                     path_List_Over.append(path_List_Write)
-                    #path_List_Write.append('\n')
-                    path_List_Write=[]                     
+
+                    path_List_Write=[]    
+                    pathLength=0                 
                     break  
                 if(pathdic[cur][0]==0):
                     last_Flag='0'
-                    pin_List_Write.append(cur)
-                    pin_List_Write.append(first_Flag+last_Flag) 
-                    pin_List_Over.append(pin_List_Write)    
-                    pin_List_Write=[]      
-                    #pin_List_Write.append('\n')
+                    # pin_List_Write.append(cur)
+                    # pin_List_Write.append(first_Flag+last_Flag) 
+                    # pin_List_Over.append(pin_List_Write)    
+                    # pin_List_Write=[]      
+
+                    path_List_Write.append(first_Flag+last_Flag)
+                    path_List_Write.append(curId)
+                    path_List_Write.append(pathLength)
+                   
                     path_List_Over.append(path_List_Write)
-                    #path_List_Write.append('\n')
+
                     path_List_Write=[]
+                    pathLength=0
                     break                    
                 if(cur  in pinlist):
                     last_Flag='0'
-                    pin_List_Write.append(cur)
-                    pin_List_Write.append(first_Flag+last_Flag) 
-                    pin_List_Over.append(pin_List_Write)    
-                    pin_List_Write=[]       
-                    #pin_List_Write.append('\n')
+                    # pin_List_Write.append(cur)
+                    # pin_List_Write.append(first_Flag+last_Flag) 
+                    # pin_List_Over.append(pin_List_Write)    
+                    # pin_List_Write=[]       
+
+                    path_List_Write.append(first_Flag+last_Flag)
+                    path_List_Write.append(curId)
+                    path_List_Write.append(pathLength)
+                    
                     path_List_Over.append(path_List_Write)
-                    #path_List_Write.append('\n')
+
                     path_List_Write=[]
-                    pin_List_Write.append(cur)
+                    pathLength=0
+                    # pin_List_Write.append(cur)
                     path_List_Write.append(cur)
+                    pathLength+=1
                     cur=(pathdic[cur][1],pathdic[cur][2])
                     first_Flag='0'
                 else:
                     cur=(pathdic[cur][1],pathdic[cur][2])
-        #when stop while(1)?
+
 
     list1=[]   
     if(len(list0)!=0):
@@ -163,13 +229,22 @@ if __name__=='__main__':
     path_List_Write=[]
     pin_List_Write=[]   
     path_List_Over=[]
+    path_List_Over_Plot = []
     pin_List_Over=[]
-    with open('2d_Pinlist.txt', "r") as file0 , open('2d_output1.txt','r') as file1:
-        net_length=int(file0.readline().split()[0])
-        for i in range(146127):
-            line=file0.readline()
-            line=file1.readline()        
-        for iii in range(1):
+    path_List_Over_Final=[]
+    pin_List_Over_Final=[]
+    realPin={}
+    pathLength=0
+    net_length=0
+    curId = 0
+
+    with open("2d_Pinlist.txt", "r") as file0 , open("2d_output1.txt","r") as file1:
+        net_length=int(file0.readline().split()[0])                                         ##########              注意
+        # for i in range(146127):
+        #     line=file0.readline()
+        #     line=file1.readline()        
+        for iii in range(net_length):
+    
             path_List_Write=[]
             pin_List_Write=[]
             pinlist.clear()
@@ -205,9 +280,11 @@ if __name__=='__main__':
             
         
             line=file0.readline().split()
+            curId = int(line[0].replace('n',''))
             num=int(line[1])
             for j in range(num):
                 pinlist.append((int(line[2*j+2]),int(line[2*j+3])))
+                realPin[(int(line[2*j+2]),int(line[2*j+3]),curId)]=1
     
     
             
@@ -241,25 +318,33 @@ if __name__=='__main__':
                 yes1(cur)
             else:
                 first_Flag='0'
-                pin_List_Write.append(cur)
+                # pin_List_Write.append(cur)
                 #print(cur)
                 path_List_Write.append(cur)
+                pathLength+=1
                 cur=(pathdic[cur][1],pathdic[cur][2])           
     
                 while(1):
+                    pathLength+=1
                     path_List_Write.append(cur)
                     if(pathdic[cur][0]==0): #终止点一定是 pin？  需要验证
                         # if(cur in pinlist):
                         last_Flag='0'
                         # else:
                         #     last_Flag='1'
-                        pin_List_Write.append(cur)
-                        pin_List_Write.append(first_Flag+last_Flag) 
-                        pin_List_Over.append(pin_List_Write)    
-                        pin_List_Write=[]         
-                        #pin_List_Write.append('\n')
+                        # pin_List_Write.append(cur)
+                        # pin_List_Write.append(first_Flag+last_Flag) 
+                        # pin_List_Over.append(pin_List_Write)    
+                        # pin_List_Write=[]         
+    
+                        path_List_Write.append(first_Flag+last_Flag)
+                        path_List_Write.append(curId)
+                        path_List_Write.append(pathLength)
+                        
                         path_List_Over.append(path_List_Write)
-                        #path_List_Write.append('\n')
+    
+       
+                        pathLength=0
                         path_List_Write=[]
                         #print(0)
                         break
@@ -272,44 +357,59 @@ if __name__=='__main__':
                             last_Flag='1'
                             for i in range(pathdic[cur][0]):                        
                                 list0.append((1,cur[0],cur[1],pathdic[cur][2*i+1],pathdic[cur][2*i+2]))            #进入函数      
-                            #list0.append(pathdic[cur][2*i+2])
-                        pin_List_Write.append(cur)
-                        pin_List_Write.append(first_Flag+last_Flag) 
-                        pin_List_Over.append(pin_List_Write)    
-                        pin_List_Write=[]         
-                        #pin_List_Write.append('\n')
+    
+                        # pin_List_Write.append(cur)
+                        # pin_List_Write.append(first_Flag+last_Flag) 
+                        # pin_List_Over.append(pin_List_Write)    
+                        # pin_List_Write=[]         
+    
+                        path_List_Write.append(first_Flag+last_Flag)
+                        path_List_Write.append(curId)
+                        path_List_Write.append(pathLength)
+                        
                         path_List_Over.append(path_List_Write)
-                        #path_List_Write.append('\n')
+    
+                        
+                        pathLength=0
                         path_List_Write=[]
                         yes1(cur)
                         break  
                     if(cur  in pinlist and pathdic[cur][0]==1):
                         last_Flag='0'
-                        pin_List_Write.append(cur)
-                        pin_List_Write.append(first_Flag+last_Flag) 
-                        pin_List_Over.append(pin_List_Write)    
-                        pin_List_Write=[]         
-                        #pin_List_Write.append('\n')
+                        # pin_List_Write.append(cur)
+                        # pin_List_Write.append(first_Flag+last_Flag) 
+                        # pin_List_Over.append(pin_List_Write)    
+                        # pin_List_Write=[]         
+    
+                        path_List_Write.append(first_Flag+last_Flag)
+                        path_List_Write.append(curId)
+                        path_List_Write.append(pathLength)
+                        
                         path_List_Over.append(path_List_Write)
-                        #path_List_Write.append('\n')
+                        
+                        pathLength=0
                         path_List_Write=[]
-                        pin_List_Write.append(cur)
+                        # pin_List_Write.append(cur)
                         path_List_Write.append(cur)
+                        pathLength+=1
                         cur=(pathdic[cur][1],pathdic[cur][2])
                         first_Flag='0'
                     else:
-                        cur=(pathdic[cur][1],pathdic[cur][2])      
-            pin_List_Over.append([])     #代表将处理下一个线网
-            path_List_Over.append([])    #代表将处理下一个线网
+                        cur=(pathdic[cur][1],pathdic[cur][2])  
+            #path_List_Over.sort(key=lambda s: int(s[-1]),reverse=1)#升序,怎么一时升序，一时降序？没搞清楚
+            pathLength=0
+            # pin_List_Over_Final+=pin_List_Over
+            path_List_Over_Final+=path_List_Over    
+            pin_List_Over=[]
+            if(iii == 114642):               #plot net 
+                path_List_Over_Plot = path_List_Over
+            path_List_Over=[]
+            # pin_List_Over_Final.append([])     #代表将处理下一个线网
+            path_List_Over_Final.append([])    #代表将处理下一个线网
+            
     
-        #for i in range(20):
-        #    print(pin_List_Over[i])
-        #print('lueluelue')
-        #for i in range(20):
-        #    print(path_List_Over[i])
-        print(path_List_Over)
-        print('lueluelue')
-        print(pin_List_Over)
+    
+    plot_branch()
     
 
 
